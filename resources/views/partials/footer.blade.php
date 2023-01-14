dd8f18ddbbe9fa390d0e23c9403d2882727831ce
nembo93 <francesco.c193@libero.it>
2023-01-14 20:11:59 +0100

    Aggiunge footer top

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,32 @@
-<div>
-    <h1 class="foo">Footer</h1>
-</div>
+<section>
+    
+    <div class="footer_top">
+        <div class="footer_top_link_box">
+            <a href="" class="footer_top_link_card">
+                <div>
+                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    <h6>DIGITAL COMICS</h6>
+                </div>
+            </a>
+            <a href="" class="footer_top_link_card">
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                <h6>DC MERCHANDISES</h6>
+            </a>
+            <a href="">
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                <h6>SUBSCRIPTION</h6>
+            </a>
+            <a href="">
+                <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                <h6>COMIC SHOP LOCATOR</h6>
+            </a>
+            
+            
+        </div>
+    </div>
+
+    <div class="footer_main"></div>
+
+    <div class="footer_bottom"></div>
+
+</section>
