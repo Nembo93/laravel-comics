0705451cd7a78659f54afafcc4f2cd140f9bb122
nembo93 <francesco.c193@libero.it>
2023-01-14 19:08:36 +0100

    Aggiunge valori dell'array

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,25 @@
 @extends('layout')
 
 @section('homepage_content')
-    <h1>Ecco il main della home</h1>
+    <div class="container comics_page_container">
+
+        <div class="comics_container">
+            <div class="row justify-content-around">
+                @foreach($comics as $item)
+                <div class="col-2 comics_box">
+                    <div class="comics_img_box">
+                        <img src="{{ $item["thumb"] }}" alt="">
+                    </div>
+                    <div>
+                        <h6>{{ $item["title"] }}</h6>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="load_button_box">
+            <button class="button first_button"><strong>LOAD MORE</strong></button>
+        </div>
+        
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 $comics = config("comics_db");
-// dd($comics);
-    return view('homepage');
+
+    return view('homepage', compact("comics"));
 });
