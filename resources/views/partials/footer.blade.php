ff81e89c9f57fe96719f3b69c4416b770866281c
nembo93 <francesco.c193@libero.it>
2023-01-15 10:11:00 +0100

    Aggiunge contenuti footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <section>
-    
+    {{-- FOOTER TOP --}}
     <div class="footer_top">
         <div class="footer_top_link_box">
             <a href="" class="footer_top_link_card">
@@ -19,14 +19,71 @@
             <a href="" class="footer_top_link_card">
                 <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
                 <h6>COMIC SHOP LOCATOR</h6>
-            </a>
-            
-            
+            </a>           
         </div>
     </div>
 
-    <div class="footer_main"></div>
+    {{-- FOOTER MAIN --}}
+    <div class="footer_main">
+        {{-- Footer link section --}}
+        <div>
+            <div class="footer_main_link">
+                <div>
+                    <h5>DC COMICS</h5>
+                    <ul>
+                        <li><a href="">Characters</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">Tv</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">News</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">Movies</a></li>
+                    </ul>
+                    <h5>SHOP</h5>
+                    <ul>
+                        <li><a href="">Shop DC</a></li>
+                        <li><a href="">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+                <div class="">
+                    <h5>DC</h5>
+                    <ul>
+                        <li><a href="">Terms of news</a></li>
+                        <li><a href="">Privacy police (New)</a></li>
+                        <li><a href="">Ad choices</a></li>
+                        <li><a href="">Advertising</a></li>
+                        <li><a href="">Job</a></li>
+                        <li><a href="">Subscription</a></li>
+                        <li><a href="">Talent Workshops</a></li>
+                        <li><a href="">CPSC Certificates</a></li>
+                        <li><a href="">Rating</a></li>
+                        <li><a href="">Shop help</a></li>
+                        <li><a href="">Contact us</a></li>
+                    </ul>
+                </div>
+                <div class="">
+                    <h5>SITES</h5>
+                    <ul>
+                        <li><a href="">DC</a></li>
+                        <li><a href="">MAD Magazine</a></li>
+                        <li><a href="">DC Kids</a></li>
+                        <li><a href="">DC Universe</a></li>
+                        <li><a href="">DC Power visa</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer_main_rights">
+                <p>All Sites Content TM and c 2020 DC Entarteinement, unless otherwise <a href="">noted here</a>. All right reserved. <a href="">Cookies Settings</a></p>
+            </div>
+        </div>
+        
 
+        {{-- Foter 2 background section --}}
+    </div>
+
+    {{-- FOOTER BOTTOM --}}
     <div class="footer_bottom"></div>
 
 </section>
